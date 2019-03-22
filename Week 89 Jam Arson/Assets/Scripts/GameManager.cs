f5ee55e46edc30e2f177261705c94a400f41d697
Baradoros <thatfunderburkguy@gmail.com>
2019-03-22 18:28:15 -0500

    Cleaned up tests

--- Week 89 Jam Arson/Assets/Scripts/GameManager.cs
+++ Week 89 Jam Arson/Assets/Scripts/GameManager.cs
@@ -2,19 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    GameObject player;
+public class GameManager : MonoBehaviour {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Change
+
+    void Start() {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void Update() {
+
     }
 }
