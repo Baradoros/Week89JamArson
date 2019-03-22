1360a46af88b9b68ebf58d549601253cf80293b6
Baradoros <thatfunderburkguy@gmail.com>
2019-03-22 17:49:31 -0500

    Test change

--- Week 89 Jam Arson/Assets/Scripts/GameManager.cs
+++ Week 89 Jam Arson/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Change
     }
 
     // Update is called once per frame
