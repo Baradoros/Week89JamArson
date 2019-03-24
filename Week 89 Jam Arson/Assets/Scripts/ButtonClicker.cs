e2a0fe9c8ad5faa2b51af899f37e371cbe53ecbc
D3vmotiv <48844194+D3vmotiv@users.noreply.github.com>
2019-03-24 13:55:25 +0100

    LevelChanger

---
+++ Week 89 Jam Arson/Assets/Scripts/ButtonClicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClicker : MonoBehaviour
+{
+    //Name of the scene that is supposed to load next
+    public string sceneNameLoad;
+
+    //Animator to fade in/out between scenes 
+    public Animator animator;
+
+    //Script that is responsible for changing the scene 
+    public SceneLoader sl;
+
+    //Function that is responsible for going to the next scene after a mouse click
+    public void OnMouseDown()
+    {
+        sl.sceneName = sceneNameLoad;
+        FadeToLevel();
+    }
+        
+    void FadeToLevel()
+    {
+        animator.SetTrigger("Fade_Out");
+    }
+
+}
