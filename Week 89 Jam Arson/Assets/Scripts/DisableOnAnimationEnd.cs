9ecffa3d8a7f9e3da210660eee5dc3606f32b5fb
Baradoros <thatfunderburkguy@gmail.com>
2019-03-25 20:16:33 -0500

    Cleaned debug statements

--- Week 89 Jam Arson/Assets/Scripts/DisableOnAnimationEnd.cs
+++ Week 89 Jam Arson/Assets/Scripts/DisableOnAnimationEnd.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DisableOnAnimationEnd : StateMachineBehaviour {
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        Debug.Log("Disabling gameobject: " + animator.gameObject);
         animator.gameObject.SetActive(false);
     }
 
