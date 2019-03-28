bb4831715b32f82d739712febb37b984c40834a8
Baradoros <thatfunderburkguy@gmail.com>
2019-03-27 20:29:54 -0500

    Added music

---
+++ Week 89 Jam Arson/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager mm;
+    public AudioSource menuTheme;
+    public AudioSource gameLoop;
+
+    private void OnEnable() {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable() {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+
+    }
+
+    void Start() {
+        // Singleton
+        if (mm == null)
+            mm = this;
+        else if (mm != this)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        if (scene.name.Equals("MainMenu")) {
+            menuTheme.Play();
+            gameLoop.Stop();
+            
+        }
+        else {
+            gameLoop.Play();
+            menuTheme.Stop();
+        }
+    }
+}
