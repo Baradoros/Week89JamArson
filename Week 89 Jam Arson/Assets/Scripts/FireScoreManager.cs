8e83422a446f3cb71ef2809f036ef70508c9beab
Baradoros <thatfunderburkguy@gmail.com>
2019-03-28 00:05:50 -0500

    added fire

---
+++ Week 89 Jam Arson/Assets/Scripts/FireScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireScoreManager : MonoBehaviour
+{
+
+    public LevelManager levelManager;
+    public Image[] images;
+
+    private void Start() {
+        Debug.Log(levelManager.score);
+        for (int i = 0; i < images.Length; i++) {
+            images[i].color = Color.black;
+        }
+        for (int i = 0; i < levelManager.score; i++) {
+            images[i].color = Color.white;
+        }
+    }
+}
